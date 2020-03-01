ac60bc533c2efb9dc63e90f0f4c2cbb1b3245911
LouisDecloedt <louis.decloedt@hotmail.be>
2020-03-01 10:56:30 +0100

    DoubleVector, DoublePoint formatted:
    @Polo todo: getDrawingCommands and contains in roundedPolygon

--- src/drawit/DoublePoint.java
+++ src/drawit/DoublePoint.java
@@ -1,39 +1,27 @@
 package drawit;
 
-//no formal documentation required
+//no formal documentation required for this class
 public class DoublePoint {
 	
 	private final double xCoordinate;
 	private final double yCoordinate;
 	
+	public double getX() { return xCoordinate; }
+	public double getY() { return yCoordinate; }
 	
-	//constructor: DoublePoint(double,double)
 	public DoublePoint(double x, double y) {
 		this.xCoordinate = x;
 		this.yCoordinate = y;
 	}
-	
-	//getX()
-	public double getX() {
-		return xCoordinate;
-	}
-	
-	//getY()
-	public double getY() {
-		return yCoordinate;
-	}
-	
-	//minus(DoublePoint) -- displacement from other point to this one
+
 	public DoubleVector minus(DoublePoint minusPoint) {
 		return new DoubleVector(this.xCoordinate - minusPoint.getX(), this.yCoordinate - minusPoint.getY());
 	}
 	
-	//plus(DoubleVector)
 	public DoublePoint plus(DoubleVector displacement) {
 		return new DoublePoint(this.xCoordinate + displacement.getX(), this.yCoordinate + displacement.getY());
 	}
 	
-	//round() !!nearest integer
 	public IntPoint round() {
 		return new IntPoint(Math.toIntExact(Math.round(this.xCoordinate)),Math.toIntExact(Math.round(this.yCoordinate)));
 	}
--- src/drawit/DoubleVector.java
+++ src/drawit/DoubleVector.java
@@ -1,55 +1,40 @@
 package drawit;
 
-//no formal documentation required
+//no formal documentation required for this class
 public class DoubleVector {
 	
 	private final double xCoordinate;
 	private final double yCoordinate;
 	
-	//constructor: DoubleVector(double,double)
+	public double getX() { return this.xCoordinate; }
+	public double getY() { return this.yCoordinate; }
+		
 	public DoubleVector(double xCoordinate, double yCoordinate) {
 		this.xCoordinate = xCoordinate;
 		this.yCoordinate = yCoordinate;
 	}
 	
-	//getX()
-	public double getX() {
-		return this.xCoordinate;
-	}
-	
-	//getY()
-	public double getY() {
-		return this.yCoordinate;
-	}
-	
-	//asAngle
-	//returns angle between -PI/PI
+	//returns angle between -PI and PI
 	public double asAngle() {
 		return Math.atan2(this.getY(), this.getX());
 	}
 	
-	//crossProduct(DoubleVector)
 	public double crossProduct(DoubleVector vector) {
 		return this.xCoordinate*vector.getY() - this.yCoordinate*vector.getX();
 	}
 	
-	//dotProduct(DoubleVector)
 	public double dotProduct(DoubleVector vector) {
 		return this.xCoordinate*vector.getX() + this.yCoordinate*vector.getY();
 	}
 	
-	//getSize() //lengte??
 	public double getSize() {
 		return Math.sqrt(Math.pow(this.xCoordinate,2) + Math.pow(this.yCoordinate,2));		
 	}
 	
-	//plus(DoubleVector)
 	public DoubleVector plus(DoubleVector vector) {
 		return new DoubleVector(this.xCoordinate+vector.getX(), this.yCoordinate+vector.getY());
 	}
 	
-	
-	//scale(double)
 	public DoubleVector scale(double scalar) {
 		return new DoubleVector(this.xCoordinate*scalar, this.yCoordinate*scalar);
 	}

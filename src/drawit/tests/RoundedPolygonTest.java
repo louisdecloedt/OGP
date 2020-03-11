6d9f0e5ffe1e5ab8c9bd04b0091c999b91ba501c
LouisDecloedt <louis.decloedt@hotmail.be>
2020-03-11 08:57:32 +0100

    everything should work now, do not hand in yet

--- src/drawit/tests/RoundedPolygonTest.java
+++ src/drawit/tests/RoundedPolygonTest.java
@@ -1,7 +1,7 @@
 package drawit.tests;
 
 import static org.junit.jupiter.api.Assertions.*;
-//import org.junit.*;
+
 
 import org.junit.jupiter.api.Test;
 
@@ -12,7 +12,7 @@ import drawit.RoundedPolygon;
 public class RoundedPolygonTest {
 
 	
-	@org.junit.Test 
+	@Test 
 	public void test() { 
 		IntPoint point1 = new IntPoint(0,0);
 	    IntPoint point2 = new IntPoint(100,0); 
@@ -30,6 +30,8 @@ public class RoundedPolygonTest {
 	    assertEquals(true, array1[2].equals(point3)); 
 	    assertEquals(true, array1[3].equals(point4));
 	    assertEquals(null, PointArrays.checkDefinesProperPolygon(array1));
+	    
+	    poly1.setRadius(1);
 	  
 	    poly1.setVertices(array1); 
 	    IntPoint testPoint1 = new IntPoint(10,10);
@@ -168,7 +170,7 @@ public class RoundedPolygonTest {
 		assertEquals(true, IntPoint.lineSegmentsIntersect(testPoint9, exitPoint, point10, point11));
 	}
 	
-	@org.junit.Test
+	@Test
 	public void getDrawingCommands() {
 		RoundedPolygon poly1 = new RoundedPolygon();
 		//System.out.print(poly1.getDrawingCommands());
@@ -188,7 +190,7 @@ public class RoundedPolygonTest {
 
 	}
 	
-	@org.junit.Test
+	@Test
 	public void testConcave() {
 		/**
 		 * Test cases for Rounded Polygon
@@ -196,15 +198,11 @@ public class RoundedPolygonTest {
 
 
 		IntPoint punt1 = new IntPoint(0, 0);
-		IntPoint punt2 = new IntPoint(3, 0);
-		IntPoint punt3 = new IntPoint(4, 1);
-		IntPoint punt4 = new IntPoint(5, 0);
-		IntPoint punt5 = new IntPoint(7, 5);
-		IntPoint punt6 = new IntPoint(5, 10);
-		IntPoint punt7 = new IntPoint(6, 4);
-		IntPoint punt8 = new IntPoint(3, 7);
-		IntPoint punt9 = new IntPoint(0, 7);
-		IntPoint punt10 = new IntPoint(1, 4);
+		IntPoint punt2 = new IntPoint(30, 0);
+		IntPoint punt3 = new IntPoint(20, 5);
+		IntPoint punt4 = new IntPoint(30, 10);
+		IntPoint punt5 = new IntPoint(0, 10);
+		IntPoint punt6 = new IntPoint(5, 5);
 
 		IntPoint[] lijst = new IntPoint[0];
 		lijst = PointArrays.insert(lijst, 0, punt1);
@@ -213,10 +211,6 @@ public class RoundedPolygonTest {
 		lijst = PointArrays.insert(lijst, 3, punt4);
 		lijst = PointArrays.insert(lijst, 4, punt5);
 		lijst = PointArrays.insert(lijst, 5, punt6);
-		lijst = PointArrays.insert(lijst, 6, punt7);
-		lijst = PointArrays.insert(lijst, 7, punt8);
-		lijst = PointArrays.insert(lijst, 8, punt9);
-		lijst = PointArrays.insert(lijst, 9, punt10);
 
 
 		RoundedPolygon roundpoly = new RoundedPolygon();
@@ -224,16 +218,18 @@ public class RoundedPolygonTest {
 
 		assertEquals(null, PointArrays.checkDefinesProperPolygon(lijst));
 
-		IntPoint testPunt1 = new IntPoint(3, 0);
-		IntPoint testPunt2 = new IntPoint(0, 4);
-		IntPoint testPunt3 = new IntPoint(4, 10);
-		IntPoint testPunt4 = new IntPoint(4, 2);
-		IntPoint testPunt5 = new IntPoint(-1, 7);
+		IntPoint testPunt1 = new IntPoint(10, 5);
+		IntPoint testPunt2 = new IntPoint(3, 5);
+		IntPoint testPunt3 = new IntPoint(2, 6);
+		IntPoint testPunt3B = new IntPoint(3, 4);
+		IntPoint testPunt4 = new IntPoint(10, 6);
+		IntPoint testPunt5 = new IntPoint(25, 5);
 
 
 		assertEquals(true, roundpoly.contains(testPunt1));
 		assertEquals(false, roundpoly.contains(testPunt2));
 		assertEquals(false, roundpoly.contains(testPunt3));
+		assertEquals(false, roundpoly.contains(testPunt3B));
 		assertEquals(true, roundpoly.contains(testPunt4));
 		assertEquals(false, roundpoly.contains(testPunt5));
 		roundpoly.contains(testPunt2);

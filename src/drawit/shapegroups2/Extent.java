b2829b022b91c4eea7c7ffb33a810b20c5dfb345
LouisDecloedt <louis.decloedt@hotmail.be>
2020-03-21 10:27:00 +0100

    function headers drawit.shapegroups2

---
+++ src/drawit/shapegroups2/Extent.java
@@ -0,0 +1,71 @@
+package drawit.shapegroups2;
+
+import drawit.IntPoint;
+
+public class Extent {
+	
+	public int getLeft() {
+		
+	}
+	
+	public int getTop() {
+		
+	}
+	
+	public int getRight() {
+		
+	}
+	
+	public int getBottom() {
+		
+	}
+	
+	public int getWidth() {
+		
+	}
+	
+	public int getHeight() {
+		
+	}
+	
+	public IntPoint getTopLeft() {
+		
+	}
+	
+	public IntPoint getBottomRight() {
+		
+	}
+	
+	public static Extent ofLeftTopWidthHeight(int left, int top, int width, int height) {
+		
+	}
+	
+	public static Extent ofLeftTopRightBottom(int left, int top, int right, int bottom) {
+		
+	}
+	
+	public Extent withLeft(int newLeft) {
+		
+	}
+	
+	public Extent withTop(int newTop) {
+		
+	}
+	
+	public Extent withRight(int newRight) {
+		
+	}
+	
+	public Extent withBottom(int newBottom) {
+		
+	}
+	
+	public Extent withWidth(int newWidth) {
+		
+	}
+	
+	public Extent withHeight(int newHeight) {
+		
+	}
+
+}
---
+++ src/drawit/shapegroups2/ShapeGroup.java
@@ -0,0 +1,72 @@
+package drawit.shapegroups2;
+
+public class ShapeGroup {
+	
+	public ShapeGroup(RoundedPolygon shape) {
+		
+	}
+	
+	public ShapeGroup(ShapeGroup[] subgroups) {
+		
+	}
+	
+	public Extent getExtent() {
+		
+	}
+	
+	public Extent getOriginalExtent() {
+		
+	}
+	
+	public ShapeGroup getParentGroup() {
+		
+	}
+	
+	public RoundedPolygon getShape() {
+		
+	}
+	
+	public java.util.List<ShapeGroup> getSubGroups() {
+		
+	}
+	
+	public int getSubgroupCount() {
+		
+	}
+	
+	public ShapeGroup getSubgroup(int index) {
+		
+	}
+	
+	public IntPoint toInnerCoordinates(IntPoint globalCoordinates) {
+		
+	}
+	
+	public IntPoint toGlobalCoordinates(IntPoint innerCoordinates) {
+		
+	}
+	
+	public IntVector toInnerCoordinates(IntVector relativeGlobalCoordinates) {
+		
+	}
+	
+	public ShapeGroup getSubGroupAt(IntPoint innerCoordinates) {
+		
+	}
+	
+	public void setExtent(Extent newExtent) {
+		
+	}
+	
+	public void bringToFront() {
+		
+	}
+	
+	public void sendToBack() {
+		
+	}
+	
+	public java.lang.String GetDrawingCommands(){
+		
+	}
+}
